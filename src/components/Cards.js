1be70aef99dd24c0fbd1ac7d9cf1e855f9a7c168
Sarah Jacobs <sgjacobs14@gmail.com>
2023-08-17 14:07:14 -0400

    fixed spelling

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -17,7 +17,7 @@ function Cards() {
                     <p className='py-2 border-b mx-8'>No storage</p>
                     <p className='py-2 border-b mx-8'>Send up to 2 GB a month</p>
                 </div>
-                <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trail</button>
+                <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
             </div>
             <div className='w-full p-4 flex flex-col my-8 md:my-0 bg-gray-100 rounded-lg hover:scale-105 duration-300 shadow-xl'>
                 <img className='w-20 mx-auto mt-[-3rem] bg-transparent ' src={Image2} alt="" /> 
@@ -39,7 +39,7 @@ function Cards() {
                     <p className='py-2 border-b mx-8'>500GB Storage</p>
                     <p className='py-2 border-b mx-8'>Monthly Consultations</p>
                 </div>
-                <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trail</button>
+                <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
             </div>
         </div>
     </div>
